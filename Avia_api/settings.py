c4654dd1dae71a6857da6ef7a53f515b26c49130
kurmanjan2506 <kurmanjan25nurbekova@gmail.com>
2022-10-17 12:25:31 +0600

    tickets

--- Avia_api/settings.py
+++ Avia_api/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt.token_blacklist',
     'django_filters',
     'drf_yasg',
-    # my_apps
+    # my_apps,
+    'tickets',
 
 ]
 
@@ -97,7 +98,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
-AUTH_USER_MODEL = 'account.CustomUser'
+# AUTH_USER_MODEL = 'account.CustomUser'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
