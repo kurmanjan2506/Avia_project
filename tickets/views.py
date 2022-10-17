c71dab194773801dd5d9b16ef780b2e32db59ea7
kurmanjan2506 <kurmanjan25nurbekova@gmail.com>
2022-10-17 14:14:24 +0600

    z

--- tickets/views.py
+++ tickets/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import permissions
+from rest_framework.viewsets import ModelViewSet
+from .models import Tickets
+from . import serializers
 
-# Create your views here.
+
+class TicketViewSet(ModelViewSet):
+    queryset = Tickets.objects.all()
+    serializer_class = serializers.TicketSerializers
+
+    def get_permissions(self):
+        if self.action in ('retrieve', 'list'):
+            return (permissions.AllowAny(),)
+        else:
+            return (permissions.IsAdminUser(),)
